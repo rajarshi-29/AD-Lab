add19fb739c2df48367d65d02337fee4a3660e2b
Rajarshi Mukherjee <rajarshi.mukherjee2904@gmail.com>
2026-01-07 12:39:06 +0530

    AD Lab Codes

---
+++ Lab_4A/q1/script.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("button");
+btn.addEventListener("click", (e) => {
+  const dobInput = document.getElementById("dob").value;
+  const result = document.getElementById("result");
+
+  if (!dobInput) {
+    result.innerText = "Please select a date!";
+    return;
+  }
+
+  const dob = new Date(dobInput);
+  const today = new Date();
+
+  let years = today.getFullYear() - dob.getFullYear();
+  let months = today.getMonth() - dob.getMonth();
+  let days = today.getDate() - dob.getDate();
+  if (days < 0) {
+    months--;
+    const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    days += prevMonth.getDate();
+  }
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  result.innerText = `You are ${years} years, ${months} months, and ${days} days old.`;
+});
